037859cee3bf708e52ea656d11a6528dec1c5ced
Leonardo De la cruz <leonardodelacruz03@hotmail.com>
2023-03-15 10:00:35 -0500

    create a server web and welcome route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn welcome() -> impl Responder {
+    format!("Actix and PostgreSQL")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(welcome)
+    })
+    .bind(("127.0.0.1", 4000))?
+    .run().await
 }
